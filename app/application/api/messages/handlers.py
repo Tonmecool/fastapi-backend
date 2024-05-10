74cea2ef58c8c79d2c82cb45b751a6624d4f6c16
Tonmecool <qpwoei.86@mail.ru>
2024-05-10 15:58:38 +0700

    add pre-commit and linter

--- app/application/api/messages/handlers.py
+++ app/application/api/messages/handlers.py
@@ -35,7 +35,6 @@ router = APIRouter(tags=['Chat'])
 
 @router.post(
     '/', 
-    # response_model=CreateChatResponseSchema, 
     status_code=status.HTTP_201_CREATED,
     description='endpoint creates a new chat. if the chat already exists, an exception 400 is raised',
     responses={
@@ -43,7 +42,10 @@ router = APIRouter(tags=['Chat'])
         status.HTTP_400_BAD_REQUEST: {'model': ErrorSchema},
     }
 )
-async def create_chat_handler(schema: CreateChatRequestSchema, container=Depends(init_container)) -> CreateChatResponseSchema:
+async def create_chat_handler(
+    schema: CreateChatRequestSchema, 
+    container: Container = Depends(init_container)
+) -> CreateChatResponseSchema:
     ''' Creates a new chat '''
     mediator = container.resolve(Mediator)
 
@@ -53,7 +55,6 @@ async def create_chat_handler(schema: CreateChatRequestSchema, container=Depends
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={'error': exception.message})
 
     return CreateChatResponseSchema.from_entity(chat)
-
 
 @router.post(
     '/{chat_oid}/messages', 
