e1543cd59e61c472842b3fced8b9815b4c320800
Tonmecool <qpwoei.86@mail.ru>
2024-05-10 18:25:13 +0700

    add notifications

---
+++ app/infra/integrations/notifications/clients/base.py
@@ -0,0 +1,19 @@
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from dataclasses import dataclass
+
+from infra.integrations.notifications.dtos import Notification
+
+
+@dataclass
+class BaseNotificationClient(ABC):
+
+    @abstractmethod
+    async def _format_notification(self, notification: Notification) -> str:
+        ...
+
+    @abstractmethod
+    async def send(self, notification: Notification):
+        ...
